9cf5fe03c94d11c33e19407bf377b9cbe6e8c9ec
Lucas Bonadio <lucasbonadio2@hotmail.com>
2022-05-13 14:56:11 -0300

    Quebra de Linhas nos Textos

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -52,8 +52,8 @@ function Panel({ updateAt, onChange, data, country, getCovidData }) {
         <Card>
             <CardPanelContentStyled>
                 <div>
-                    <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-                    <Typography variant="h6" component="span" color="primary">Painel Coronavirus</Typography>
+                    <Typography variant="h5" component="span" color="primary">COVID19<br/></Typography>
+                    <Typography variant="h6" component="span" color="primary">DADOS DO CORONAVIRUS<br/></Typography>
                     <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
                     <div className='pt-2'>
                         <Select onChange={onChange} value={country}>
