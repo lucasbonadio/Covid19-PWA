7d3ce39c503255b89221e791237108b6f6515a80
Lucas Bonadio <lucasbonadio2@hotmail.com>
2022-05-13 15:29:54 -0300

    Add //eslint-disable-next-line

--- src/api.js
+++ src/api.js
@@ -11,6 +11,9 @@ function getCountry(country) {
     .then((response) => response.json())
 }
 
-export default {
+const logger = {
     getCountry
-}
+};
+
+export default logger;
+
--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -1,5 +1,4 @@
 import React, { memo } from 'react'
-import RefreshIcon from '../../../assets/images/refresh.png'
 import { Card, Typography, Button, Select, MenuItem } from '../../../components'
 import COUNTRIES from '../../../commons/constants/countries'
 import { CardPanelContentStyled, ItemStyled } from './style'
@@ -7,6 +6,7 @@ import { CardPanelContentStyled, ItemStyled } from './style'
 const navigatorHasShare = navigator.share
 
 function Panel({ updateAt, onChange, data, country, getCovidData }) {
+    //eslint-disable-next-line
     const { cases, recovered, deaths, todayCases, TodayDeaths } = data
 
     const renderCountries = (country, index) => (
